d93de60db2838ee2b9c8983e4962a9dc15889452
ivanmmurciaua <imm117@alu.ua.es>
2022-05-04 22:27:11 +0200

    Initial commit

---
+++ contracts/My1155.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC1155/ERC1155.sol";
+import "./ERC1155/extensions/ERC1155Supply.sol";
+import "./ERC1155/extensions/ERC1155ApprovalById.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract My1155 is ERC1155, Ownable, Pausable, ERC1155Supply, ERC1155ApprovalById {
+    constructor() ERC1155("") {}
+
+    event Minted(address to, uint256 id, uint256 amount, bytes data);
+    event BatchMinted(address to, uint256[] ids, uint256[] amounts, bytes data);
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+        emit Minted(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+        emit BatchMinted(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
---
+++ test/1_test.js
@@ -0,0 +1,153 @@
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+
+// Test suite ERC1155ApprovalById
+describe("ERC1155ApprovalById", function(){
+  this.beforeAll(async function(){
+    try{
+      provider = waffle.provider;
+      [owner,u1,u2] = await ethers.getSigners();
+
+      //1155
+      const __1155 = await ethers.getContractFactory("My1155");
+      _1155 = await __1155.deploy();
+      await _1155.deployed();
+    } 
+    catch(ex){
+      console.error(ex);
+    }
+  });
+
+  it("Mint 100 of 0,1,2 from 1155 to Owner", async function(){
+    const ids = [ 0, 1, 2 ]
+    const amounts = [ 100, 100, 100 ]; 
+
+    await expect(_1155.mintBatch(owner.address, ids, amounts, "0x00"),"mint 1155 to owner").to.emit(_1155, "BatchMinted");
+    const expected = 100;
+
+    const realValue0 = await _1155.balanceOf(owner.address,0);
+    const realValue1 = await _1155.balanceOf(owner.address,1);
+    const realValue2 = await _1155.balanceOf(owner.address,2);
+
+    expect(realValue0).to.equal(expected);
+    expect(realValue1).to.equal(expected);
+    expect(realValue2).to.equal(expected);
+  });
+
+  describe("setApprovalById", function(){
+    it("Approve id 0 for U1", async function(){
+      const operator = u1.address;
+      const id = 0;
+      const approved = true;
+      await expect(_1155.connect(owner).setApprovalById(operator,id,approved)).to.emit(_1155, "ApprovalById");
+    });
+
+    it("Should revert when owner try to approve himself", async function(){
+      const operator = owner.address;
+      const id = 0;
+      const approved = true;
+      await expect(_1155.connect(owner).setApprovalById(operator,id,approved)).to.be.revertedWith("ERC1155ApprovalById: setting approval status for self");
+    });
+  });
+
+  describe("safeTransferFromById", function(){
+    it("Should revert when caller isn't the from param", async function(){
+      const from = u1.address;
+      const to = u2.address;
+      const id = 0;
+      const amount = 10;
+      const data = 0x00;
+      await expect(_1155.connect(owner).safeTransferFromById(from, to, id, amount, data)).to.be.revertedWith("ERC1155ApprovalById: caller is not owner nor approved for this id");
+    });
+
+    it("Should revert when id to transfer is not approved and caller != from", async function(){
+      const from = owner.address;
+      const to = u1.address;
+      const id = 1;
+      const amount = 10;
+      const data = 0x00;
+      await expect(_1155.connect(u2).safeTransferFromById(from, to, id, amount, data)).to.be.revertedWith("ERC1155ApprovalById: caller is not owner nor approved for this id");
+    });
+
+    it("Transfer 10 1155-0 to U2 by owner", async function(){
+      const from = owner.address;
+      const to = u2.address;
+      const id = 0;
+      const amount = 10;
+      const data = 0x00;
+      await expect(_1155.connect(owner).safeTransferFromById(from, to, id, amount, data),'Transfer 10-0 to U2 by owner').to.emit(_1155, "TransferSingle");
+      expect(await _1155.balanceOf(to, id),'New balance of U2').to.equal(amount);
+    });
+
+    it("Transfer 10 1155-0 to U1 call by himself", async function(){
+      const from = owner.address;
+      const to = u1.address;
+      const id = 0;
+      const amount = 10;
+      const data = 0x00;
+      await expect(_1155.connect(u1).safeTransferFromById(from, to, id, amount, data),'Transfer 10-0 to U2 by owner').to.emit(_1155, "TransferSingle");
+      expect(await _1155.balanceOf(to, id),'New balance of U1').to.equal(amount);
+    });
+  });
+
+  describe("safeBatchTransferFromById", function(){
+    it("Should be reverted because Id 1 is not approved for U1", async function(){
+      const from = owner.address;
+      const to = u1.address;
+      const amount = 10;
+      const ids = [ 0, 1 ];
+      const amounts = [ amount, amount ];
+      const data = 0x00;
+      await expect(_1155.connect(u1).safeBatchTransferFromById(from, to, ids, amounts, data)).to.be.revertedWith("ERC1155ApprovalById: transfer caller is not approved for id");
+    });
+
+    it("Transfer 10 of 0 and 1 from owner to U2", async function(){
+      // safeTransfer
+      const from = owner.address;
+      const to = u2.address;
+      const amount = 10;
+      const ids = [ 0, 1 ];
+      const amounts = [ amount, amount ];
+      const data = 0x00;
+      
+      const balanceBefore = [];
+      for (let i = 0; i < ids.length; i++) {
+        balanceBefore.push(await _1155.balanceOf(to, ids[i]));
+      }
+
+      await expect(_1155.connect(owner).safeBatchTransferFromById(from, to, ids, amounts, data)).to.emit(_1155, "TransferBatch");
+
+      for (let i = 0; i < ids.length; i++) {
+        expect(await _1155.balanceOf(to, ids[i]),'New balance of U1 of id ' + ids[i]).to.equal(parseInt(balanceBefore[i]) + amount);        
+      }
+    });
+
+    it("Approve for Id 1 and transfer 10 of 0 and 1 to U1", async function(){
+      // Approve
+      const operator = u1.address;
+      const id = 1;
+      const approved = true;
+      await expect(_1155.connect(owner).setApprovalById(operator,id,approved)).to.emit(_1155, "ApprovalById");
+
+      // safeTransfer
+      const from = owner.address;
+      const to = u1.address;
+      const amount = 10;
+      const ids = [ 0, 1 ];
+      const amounts = [ amount, amount ];
+      const data = 0x00;
+      
+      const balanceBefore = [];
+      for (let i = 0; i < ids.length; i++) {
+        balanceBefore.push(await _1155.balanceOf(to, ids[i]));
+      }
+
+      await expect(_1155.connect(u1).safeBatchTransferFromById(from, to, ids, amounts, data)).to.emit(_1155, "TransferBatch");
+
+      for (let i = 0; i < ids.length; i++) {
+        expect(await _1155.balanceOf(to, ids[i]),'New balance of U1 of id ' + ids[i]).to.equal(parseInt(balanceBefore[i]) + amount);        
+      }
+    });
+  });
+
+});
